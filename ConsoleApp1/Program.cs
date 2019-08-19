a84ebc5b80279fb0d74ef23fc6b6efe68737210a
Tyrese Samuel <s198046@students.aie.edu>
2019-08-19 16:06:37 -0500

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for( int count = 3; count >= 0; count-=1)
+            {
+                Console.WriteLine("The value of count is now " + count + ".");
+                Console.ReadKey();
+                if (count == 2)
+                {
+                    break;
+                }
+            }
+            Console.WriteLine("The loop is now finished.");
+            Console.ReadKey();
+        }
+    }
+}
